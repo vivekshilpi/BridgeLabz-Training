7122e2ecbf72f76b38de337625c0f204a68e9e8b
vivekshilpi <vivekshilpi1234@gmail.com>
2025-12-16 23:33:38 +0530

    [Vivek] : Basic Java Program

--- gcr-codebase/programming-prerequisites/AddTwoNumbers.java
+++ gcr-codebase/programming-prerequisites/AddTwoNumbers.java
@@ -1,15 +1,24 @@
-import java.util.*;
+//including scanner package to take input
+import java.util.Scanner;
 
 public class AddTwoNumbers{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first No : ");
-		int num1 = sc.nextInt();
-		System.out.println("Enter second No : ");
-		int num2 = sc.nextInt();
+		Scanner input = new Scanner(System.in);
+		
+		//Taking number1 as input from user
+		System.out.println("Enter number1: ");
+		int number1  = input.nextInt();
+		
+		//Taking number2 as input from user
+		System.out.println("Enter number2: ");
+		int number2  = input.nextInt();
 
-		int sum = num1+num2;
+		int sum = number1+number2;
 
+		//printing sum
 		System.out.println("Sum of two numbers is : "+ sum);
+
+		//closing scanner
+		input.close();
 	}
 }
--- gcr-codebase/programming-prerequisites/TemperatureConversion.java
+++ gcr-codebase/programming-prerequisites/TemperatureConversion.java
@@ -1,11 +1,20 @@
-import java.util.*;
+//including scanner package to take input
+import java.util.Scanner;
 
 public class TemperatureConversion{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Temperature in Celsius : ");
-		int celsius = sc.nextInt();
-		int Fahrenheit = (celsius * 9/5) + 32;
-		System.out.println("In Fahrenheit: "+ Fahrenheit);
+		Scanner input = new Scanner(System.in);
+		
+		//Taking temperature in celsius as input from user
+		System.out.println("Enter temperature in celsius: ");
+		int celsius  = input.nextInt();
+		
+		//calculating temperature in fahrenheit
+		int fahrenheitResult  = (celsius  * 9/5) + 32;
+		
+		//printing the temperature in fahrenheit
+		System.out.println("The "+ celsius  +" celsius in fahrenheit is "+ fahrenheitResult +" fahrenheit");
+		
+		input.close();
 	}
 }
