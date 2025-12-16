7f28fac0fcf655ac443d6ad7f7f599ed98113852
vivekshilpi <vivekshilpi1234@gmail.com>
2025-12-17 00:47:18 +0530

    [Vivek] : Program for Average of three numbers

---
+++ gcr-codebase/programming-prerequisites/AverageOfThree.java
@@ -0,0 +1,16 @@
+public class AverageOfThree{
+      public static void main(String[] args){           
+             int num1 = 94;
+           
+             int num2 = 95;
+           
+             int num3 = 96;
+
+           //sum of three numbers 
+             int total = num1 + num2 + num3;;
+
+           //calculating the average 
+             int average =  total/ 3;
+             System.out.println("Average is "+ average);
+       }
+}
