9c399ed6dcfefabb86614d75e700e97e789bb56f
vivekshilpi <vivekshilpi1234@gmail.com>
2025-12-17 16:07:23 +0530

    [Vivek] : Program for finding Average Marks

---
+++ gcr-codebase/programming-elements/Level1/AverageMarks.java
@@ -0,0 +1,12 @@
+// Writing java code to find average marks.
+public class AverageMarks{
+	public static void main(String[] args){
+        // Creating variables to indicate marks of Sam in PCM
+		int mathMarks = 94, physicsMarks = 95, chemistryMarks = 96;
+        // Finding average marks
+		int averageMarks = (mathMarks+physicsMarks+chemistryMarks)/3;
+        
+        // Printing Result
+		System.out.println("Sam’s average mark in PCM is " + averageMarks);
+	}
+}
