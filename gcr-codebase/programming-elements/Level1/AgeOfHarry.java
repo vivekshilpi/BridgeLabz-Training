103d6bdcb502e68cdeadced5cf37f729b10917c3
vivekshilpi <vivekshilpi1234@gmail.com>
2025-12-17 13:03:41 +0530

    [Vivek] : Added initial Setup

---
+++ gcr-codebase/programming-elements/Level1/AgeOfHarry.java
@@ -0,0 +1,16 @@
+// Writing java program for finding age of harry
+
+public class AgeOfHarry{
+	public static void main(String[] args){
+
+		// Create a variable birthYear to indicate the birth year of harry
+		int birthYear = 2000;
+
+		// Create a variable currentYear to indicate the birth year of harry
+		int currentYear = 2024;
+        
+        // printing Harry's present age
+		System.out.println("Harry's age in 2024 is " + (currentYear - birthYear));
+
+	}
+}
