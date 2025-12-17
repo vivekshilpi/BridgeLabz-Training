4b7f8b2e1fa484a3cd63641a2e8b23e2a5232c84
vivekshilpi <vivekshilpi1234@gmail.com>
2025-12-17 16:19:58 +0530

    [Vivek] : Program to convert Kilometers to miles

---
+++ gcr-codebase/programming-elements/Level1/KilometersToMiles.java
@@ -0,0 +1,12 @@
+// Writing Java Program to convert kilometers to miles 
+public class KilometersToMiles{
+      public static void main(String[] args){
+            //creating a double variable to store distance in kilometers which is in decimal
+             double distanceInKilometers = 10.8;
+            //creating a double variable to calculate distance in kilometers which may come in decimal
+             double distanceInMiles = 10.8 * 0.621371;
+
+             // Displaying result
+             System.out.println("The distance " + distanceInKilometers + " km in miles is " + distanceInMiles);
+       }
+}
