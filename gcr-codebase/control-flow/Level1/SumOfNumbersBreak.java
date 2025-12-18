65536ef3d034f0d237aad474d8a450733ef6afa2
vivekshilpi <vivekshilpi1234@gmail.com>
2025-12-19 01:19:46 +0530

    [Vivek] : Program to find sum of numbers until the user enters 0 or -ve

---
+++ gcr-codebase/control-flow/Level1/SumOfNumbersBreak.java
@@ -0,0 +1,33 @@
+//importing the Scanner class
+import java.util.Scanner;
+public class SumOfNumbersBreak{
+	public static void main(String[]args){
+            
+		//Create an instance of Scanner class to take user inputs
+		Scanner input = new Scanner(System.in);
+			
+		//Create a Double variable named UserValue and take input from user
+		System.out.println("Enter Value: ");
+		double userValue = input.nextDouble();	
+		
+		
+		// creating a variable of type double to store the sum
+		double total = 0.0;
+		
+		//using while loop to check if the user entered value is 0 or not
+		while(true){
+		
+		    // if value is zero or in negative break loop
+		    if(userValue <= 0){
+		        break;
+		    }
+		    total += userValue;
+			System.out.println("Enter Value again: ");
+			userValue = input.nextDouble();
+		}
+		//Displaying total value
+		System.out.println("Your total Sum is "+ total);
+	    //Closing the scanner object
+	    input.close();
+	}
+}
