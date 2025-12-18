d427fff329cf3417bdaad33e54c596fd1f30f185
vivekshilpi <vivekshilpi1234@gmail.com>
2025-12-19 01:36:05 +0530

    [Vivek] : Program to find factorial

---
+++ gcr-codebase/control-flow/Level1/FactorialNumber.java
@@ -0,0 +1,28 @@
+//importing the Scanner class for taking user inputs
+import java.util.Scanner;
+public class FactorialNumber{
+	public static void main(String[]args){
+            
+		//Create an instance of Scanner class to take user inputs
+		Scanner input = new Scanner(System.in);
+			
+		//Create a int variable named number and take input from user
+		System.out.println("Enter number: ");
+		int number = input.nextInt();
+		
+		int factorial = 0;
+		
+		if(number > 0){
+			factorial = 1;
+			while(number > 0){
+				factorial = factorial*number;
+				number--;
+			}
+		}
+		//printing result 
+		System.out.println("Factorial of number is " + factorial);
+		
+	    //Closing the scanner object
+	    input.close();
+	}
+}
