c2d35c99404c5def4babb22a7251cea36834f52a
vivekshilpi <vivekshilpi1234@gmail.com>
2025-12-22 08:04:23 +0530

    [Vivek] : Program to check number

---
+++ gcr-codebase/methods/Level2/CheckNumber.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class CheckNumber {
+    public static boolean isPositive(int number) {
+        return number >= 0;
+    }
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+    public static int compare(int number1, int number2) {
+        if (number1 > number2) {
+            return 1;
+        } else if (number1 == number2) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (isPositive(numbers[i])) {
+                System.out.println("Number " + numbers[i] + " is positive.");
+                if (isEven(numbers[i])) {
+                    System.out.println("Number " + numbers[i] + " is even.");
+                } else {
+                    System.out.println("Number " + numbers[i] + " is odd.");
+                }
+            } else {
+                System.out.println("Number " + numbers[i] + " is negative.");
+            }
+        }
+
+        int comparisonResult = compare(numbers[0], numbers[numbers.length - 1]);
+        if (comparisonResult == 1) {
+            System.out.println("The first number is greater than the last number.");
+        } else if (comparisonResult == 0) {
+            System.out.println("The first number is equal to the last number.");
+        } else {
+            System.out.println("The first number is less than the last number.");
+        }
+        
+        //close scanner
+        scanner.close();
+    }
+}
