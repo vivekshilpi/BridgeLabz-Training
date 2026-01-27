fbe154693dca24d72346fce8ad3020de93484de9
vivekshilpi <vivekshilpi1234@gmail.com>
2026-01-27 08:36:58 +0530

    [Vivek] : Added exception handling program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/junit/exceptionhandling/MathUtils.java
@@ -0,0 +1,10 @@
+package junit.exceptionhandling;
+
+public class MathUtils {
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
