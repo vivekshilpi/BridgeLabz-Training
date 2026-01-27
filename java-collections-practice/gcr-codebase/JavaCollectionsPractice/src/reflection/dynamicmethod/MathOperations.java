962c359eaa67a8fe2faf4db55d597f52a7379852
vivekshilpi <vivekshilpi1234@gmail.com>
2026-01-27 15:23:37 +0530

    [Vivek] : Added dynamicmethod program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/reflection/dynamicmethod/MathOperations.java
@@ -0,0 +1,16 @@
+package reflection.dynamicmethod;
+
+public class MathOperations {
+
+    public int add(int a, int b) {
+	return a + b;
+    }
+
+    public int subtract(int a, int b) {
+	return a - b;
+    }
+
+    public int multiply(int a, int b) {
+	return a * b;
+    }
+}
